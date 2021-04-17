b327acf2bfd16c853a387d35b392af6c4d265aff
EmanAlzariey <55436527+EmanAlzariey@users.noreply.github.com>
2021-04-17 10:53:12 +0300

    Create output of one neuron
    Simply Multiply input with its unique weight , and add the bias
    
    Note: each neuron has only one unique bias

--- p1.py
+++ p1.py
@@ -1,12 +1,20 @@
 #### Test Import Libraries ####
 
-import sys
-import numpy as np
-import matplotlib as plt
+# import sys
+# import numpy as np
+# import matplotlib as plt
 
-print("Python:", sys.version)
-print("Numpy:", np.__version__)
-print("Matplotlib:", plt.__version__)
+# print("Python:", sys.version)
+# print("Numpy:", np.__version__)
+# print("Matplotlib:", plt.__version__)
 
 
 
+### simple output to first Neuron ###
+
+inputs = [1.2, 5.1, 2.1]
+weights = [3.1, 2.1, 8.7]
+bias = 3
+
+output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+print(output)
