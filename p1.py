510132f4f2cebe0edbc7760faf1df1f3be7d7a0f
EmanAlzariey <55436527+EmanAlzariey@users.noreply.github.com>
2021-04-17 10:50:18 +0300

    Test import library - conda environment

--- p1.py
+++ p1.py
@@ -1,2 +1,12 @@
+#### Test Import Libraries ####
+
+import sys
+import numpy as np
+import matplotlib as plt
+
+print("Python:", sys.version)
+print("Numpy:", np.__version__)
+print("Matplotlib:", plt.__version__)
+
 
 
